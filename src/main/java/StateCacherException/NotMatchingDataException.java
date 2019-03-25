7de7074d3015c9b382a1ae73eb37353c5f11c623
Giuseppe Palazzolo <palazzolo1995@gmail.com>
2019-03-25 19:48:27 +0100

    feat: add exception class

---
+++ src/main/java/StateCacherException/NotMatchingDataException.java
@@ -0,0 +1,8 @@
+package StateCacherException;
+
+public class NotMatchingDataException extends Exception {
+
+    public NotMatchingDataException(String message){
+        super (message);
+    }
+}
