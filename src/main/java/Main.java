7e756dc383a23edcb22465dedcd01d1d63a34366
Giuseppe Palazzolo <palazzolo1995@gmail.com>
2019-04-24 12:11:20 +0200

    chore: clean up

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,15 +1,32 @@
-import StateCacherEvent.*;
-import StateCacherTool.KStreamSpecificAvroSerde;
-import StateCacherTool.KafkaManager;
+import StateCacherEvent.StateCacherEvent;
+import StateCacherEvent.TokenNetworkDelta.TokenNetworkDeltaEvent;
+import io.confluent.kafka.serializers.AbstractKafkaAvroSerDeConfig;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.Topology;
+
+import java.util.Properties;
 
 public class Main {
     public static void main(String args[]) {
+        final String schemaRegistry = "http://raiden-sr-schema-registry.kafka.svc.cluster.local:8081";
 
-        KafkaManager tokenNetworkDeltaKafkaManager = new KafkaManager("TokenNetworkDelta");
-        KStreamSpecificAvroSerde tokenNetworkDeltaKStreamManager = new KStreamSpecificAvroSerde(tokenNetworkDeltaKafkaManager.getBuilder(), tokenNetworkDeltaKafkaManager.getSchemaRegistry());
-        StateCacherEvent tokenNetworkDeltaEvent = new TokenNetworkDeltaEvent("raidenEvent.TokenNetworkDelta", tokenNetworkDeltaKStreamManager);
+        Properties property = new Properties();
+        property.put(StreamsConfig.APPLICATION_ID_CONFIG, "testKafkaTransformer2");
+        property.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "raiden-kafka-headless.kafka.svc.cluster.local:9092");
+        property.put(AbstractKafkaAvroSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG, schemaRegistry);
+        property.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "latest");
+        StreamsBuilder builder = new StreamsBuilder();
 
-        tokenNetworkDeltaEvent.run();
-        tokenNetworkDeltaKafkaManager.start();
+
+        StateCacherEvent joinTest = new TokenNetworkDeltaEvent("firstTransformerTest", builder);
+        joinTest.run();
+
+        Topology topology = builder.build();
+        KafkaStreams streams = new KafkaStreams(topology, property);
+        streams.cleanUp();
+        streams.start();
     }
 }
--- src/main/java/StateCacherEvent/StateCacherEvent.java
+++ src/main/java/StateCacherEvent/StateCacherEvent.java
@@ -1,23 +1,37 @@
 package StateCacherEvent;
 
-import StateCacherTool.KStreamSpecificAvroSerde;
+import StateCacherEvent.TokenNetworkDelta.SpecificSerdeManager;
+import io.raidenmap.statecacher.TokenNetworkDelta;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.state.KeyValueBytesStoreSupplier;
+import org.apache.kafka.streams.state.KeyValueStore;
+import org.apache.kafka.streams.state.StoreBuilder;
+import org.apache.kafka.streams.state.Stores;
 
 public abstract class StateCacherEvent {
-    public StateCacherEvent(String toStreamTopic, KStreamSpecificAvroSerde kStreamSpecificAvroSerde) {
+
+    public StateCacherEvent(String toStreamTopic, StreamsBuilder builder) {
         this.toStreamTopic = toStreamTopic;
-        this.kStreamSpecificAvroSerde = kStreamSpecificAvroSerde;
-        this.kStreamSpecificAvroSerde.createSerde(this.getClass(), false);
+        this.builder = builder;
+        specificSerdeManager = new SpecificSerdeManager(schemaRegistry);
+        storeSupplier = Stores.inMemoryKeyValueStore(stateStoreName);
+        storeBuilder = Stores.keyValueStoreBuilder(storeSupplier, Serdes.String(), specificSerdeManager.getTokenNetworkDeltaSerde());
+        builder.addStateStore(storeBuilder);
     }
 
-    public abstract void run();
-    protected abstract void send();
+    public void run() {
+    }
 
-    protected KStreamSpecificAvroSerde kStreamSpecificAvroSerde;
-
+    protected SpecificSerdeManager specificSerdeManager;
+    protected StreamsBuilder builder;
+    protected final String stateStoreName = "stateStore-3";
+    protected KeyValueBytesStoreSupplier storeSupplier;
+    protected StoreBuilder<KeyValueStore<String, TokenNetworkDelta>> storeBuilder;
     protected final String toStreamTopic;
     protected final String topicTokenNetworkCreated = "raidenEvent.TokenNetworkCreated";
     protected final String topicChannelOpened = "raidenEvent.ChannelOpened";
     protected final String topicChannelClosed = "raidenEvent.ChannelClosed";
     protected final String topicChannelNewDeposit = "raidenEvent.ChannelNewDeposit";
-
+    protected final String schemaRegistry = "http://raiden-sr-schema-registry.kafka.svc.cluster.local:8081";
 }
