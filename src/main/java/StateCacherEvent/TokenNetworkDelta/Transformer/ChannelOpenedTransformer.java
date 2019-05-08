ef84b796be340f03a4da0f0c21d2213198753e33
Giuseppe Palazzolo <palazzolo1995@gmail.com>
2019-05-08 12:07:09 +0200

    feat: add function to update UserCount state store

--- src/main/java/StateCacherEvent/TokenNetworkDelta/Transformer/ChannelOpenedTransformer.java
+++ src/main/java/StateCacherEvent/TokenNetworkDelta/Transformer/ChannelOpenedTransformer.java
@@ -2,14 +2,15 @@ package StateCacherEvent.TokenNetworkDelta.Transformer;
 
 import io.raidenmap.event.channel.ChannelOpened;
 import io.raidenmap.producerKey.ProducerKey;
-import io.raidenmap.statecacher.Channel;
-import io.raidenmap.statecacher.Key;
-import io.raidenmap.statecacher.Participant;
-import io.raidenmap.statecacher.TokenNetworkDelta;
+import io.raidenmap.statecacher.*;
 import org.apache.kafka.streams.KeyValue;
 import org.apache.kafka.streams.kstream.Transformer;
 import org.apache.kafka.streams.processor.ProcessorContext;
 import org.apache.kafka.streams.state.KeyValueStore;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ChannelOpenedTransformer extends EventTransformer implements Transformer<ProducerKey, ChannelOpened, KeyValue<Key, TokenNetworkDelta>> {
 
@@ -20,18 +21,21 @@ public class ChannelOpenedTransformer extends EventTransformer implements Transf
         super(storeName, "ChannelOpened");
     }
 
-
     @Override
     public void init(ProcessorContext context) {
         this.context = context;
         stateStore = (KeyValueStore) this.context.getStateStore(storeName);
         lightStateStore = (KeyValueStore) this.context.getStateStore(lightStoreName);
+        userCountStateStore = (KeyValueStore) this.context.getStateStore(userCountStoreName);
     }
 
     @Override
     public KeyValue<Key, TokenNetworkDelta> transform(ProducerKey producerKey, ChannelOpened channelOpened) {
         String address = channelOpened.getChannelEvent().getTokenNetworkAddress().toString();
         Key key = new Key(address);
+
+        updateUserCountStore(channelOpened);
+
         TokenNetworkDelta tokenNetworkDelta = restoreTokenNetworkDelta(key, stateStore);
         updateChannelEvent(tokenNetworkDelta, channelOpened);
         stateStore.put(key, tokenNetworkDelta);
@@ -52,12 +56,19 @@ public class ChannelOpenedTransformer extends EventTransformer implements Transf
     protected void updateChannelEvent(TokenNetworkDelta tokenNetworkDelta, Object channelEvent) {
         addChannel(tokenNetworkDelta, (ChannelOpened) channelEvent);
         incrementChannelCount(tokenNetworkDelta);
+        updateUserCount(tokenNetworkDelta);
         updateMetadata(tokenNetworkDelta, ((ChannelOpened)channelEvent).getChannelEvent());
     }
 
     private void incrementChannelCount(TokenNetworkDelta tokenNetworkDelta) {
         tokenNetworkDelta.setChannelsCount(tokenNetworkDelta.getChannelsCount() + 1);
         tokenNetworkDelta.setOpenChannels(tokenNetworkDelta.getOpenChannels() + 1);
+    }
+
+    private void updateUserCount( TokenNetworkDelta tokenNetworkDelta){
+        String tokenNetworkAddress = tokenNetworkDelta.getTokenNetworkAddress().toString();
+        UserCount userCount = userCountStateStore.get(tokenNetworkAddress);
+        tokenNetworkDelta.setUsers(userCount.getUser().size());
     }
 
     private void addChannel(TokenNetworkDelta tokenNetworkDelta, ChannelOpened channelOpened) {
@@ -80,4 +91,31 @@ public class ChannelOpenedTransformer extends EventTransformer implements Transf
         return new Participant("", 0l, 0l, false);
     }
 
+    private void updateUserCountStore(ChannelOpened channelOpened ){
+
+        String tokenNetworkAddress = channelOpened.getChannelEvent().getTokenNetworkAddress().toString();
+        UserCount userCount = userCountStateStore.get(tokenNetworkAddress);
+        String participant1 = channelOpened.getParticipant1().toString();
+        String participant2 = channelOpened.getParticipant2().toString();
+
+        if(userCount == null) {
+            userCount = new UserCount();
+            userCount.setUser(new HashMap<>());
+        }
+        userCount.getUser().put("test",0);
+        incremectParticipant(userCount.getUser(), participant1);
+        incremectParticipant(userCount.getUser(), participant2);
+
+        userCountStateStore.put(tokenNetworkAddress, userCount);
+    }
+
+    private void incremectParticipant(Map<String, Integer> userCount, String participant){
+
+        if(userCount.containsKey(participant)) {
+            int tmp = userCount.get(participant);
+            userCount.put(participant, tmp + 1);
+        }
+        else
+            userCount.put(participant, 1);
+    }
 }
