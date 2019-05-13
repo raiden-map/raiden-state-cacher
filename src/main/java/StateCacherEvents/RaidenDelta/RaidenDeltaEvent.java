6fc12bf2ac610e234a6e0362b70fd34252dfff80
Giuseppe Palazzolo <palazzolo1995@gmail.com>
2019-05-13 19:26:58 +0200

    feat: add RaidenDeltaEvent class

---
+++ src/main/java/StateCacherEvents/RaidenDelta/RaidenDeltaEvent.java
@@ -0,0 +1,34 @@
+package StateCacherEvents.RaidenDelta;
+
+import StateCacherEvents.StateStores;
+import StateCacherEvents.Topics;
+import StateCacherEvents.StateCacherEvent;
+import io.raidenmap.statecacher.Key;
+import io.raidenmap.statecacher.RaidenDelta;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.kstream.Consumed;
+import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.Printed;
+import org.apache.kafka.streams.kstream.Produced;
+
+public class RaidenDeltaEvent extends StateCacherEvent {
+
+    public RaidenDeltaEvent(StreamsBuilder builder) {
+        super(builder);
+    }
+
+    @Override
+    public void run() {
+        consumeFromTokenNetworkDeltaTopic();
+    }
+
+    private void consumeFromTokenNetworkDeltaTopic() {
+        raidenDeltaEventStream = builder.stream(Topics.tokenNetworkDeltaTopic, Consumed.with(specificSerdeManager.getKeySerde(), specificSerdeManager.getTokenNetworkDeltaSerde()))
+                .transform(TokenNetworkDeltaTransformer::new, StateStores.raidenDeltaStoreName, StateStores.userCountStoreName, StateStores.tokenNetworkDeltaStoreName);
+        raidenDeltaEventStream.print(Printed.<Key, RaidenDelta>toSysOut().withLabel("RAIDEN DELTA"));
+        raidenDeltaEventStream.to(Topics.raidenDeltaTopic, Produced.with(specificSerdeManager.getKeySerde(), specificSerdeManager.getRaidenDeltaSerde()));
+    }
+
+    private KStream<Key, RaidenDelta> raidenDeltaEventStream;
+
+}
