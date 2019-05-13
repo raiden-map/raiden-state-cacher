89a4e78a70de96ebf83e5c938e391418b62aa333
Giuseppe Palazzolo <palazzolo1995@gmail.com>
2019-05-13 19:23:52 +0200

    feat: add Topics class

---
+++ src/main/java/StateCacherEvents/Topics.java
@@ -0,0 +1,8 @@
+package StateCacherEvents;
+
+public class Topics {
+    public final static String raidenDeltaTopic = "raidenDeltaTest0";
+    public final static String raidenSnapshotTopic = "raidenSnapshotTest0";
+    public final static String tokenNetworkDeltaTopic = "tokenNetworkDeltaTest0";
+    public final static String tokenNetworkSnapshotTopic = "tokenNetworkSnapshotTest0";
+}
