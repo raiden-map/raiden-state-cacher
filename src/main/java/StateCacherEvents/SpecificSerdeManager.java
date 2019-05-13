5bd2f29ce19de9d6457bd23572ea074d7f1fabda
Giuseppe Palazzolo <palazzolo1995@gmail.com>
2019-05-13 19:30:56 +0200

    chore: add new Serdes

--- src/main/java/StateCacherEvents/SpecificSerdeManager.java
+++ src/main/java/StateCacherEvents/SpecificSerdeManager.java
@@ -1,22 +1,23 @@
-package StateCacherEvent;
+package StateCacherEvents;
 
 import io.confluent.kafka.streams.serdes.avro.SpecificAvroSerde;
+import io.raidenmap.Endpoint;
+import io.raidenmap.event.Endpoint.AddressRegistered;
 import io.raidenmap.event.channel.ChannelClosed;
 import io.raidenmap.event.channel.ChannelNewDeposit;
 import io.raidenmap.event.channel.ChannelOpened;
 import io.raidenmap.event.channel.ChannelSettled;
 import io.raidenmap.event.tokenNetwork.TokenNetworkCreated;
 import io.raidenmap.producerKey.ProducerKey;
-import io.raidenmap.statecacher.Key;
-import io.raidenmap.statecacher.TokenNetworkDelta;
-import io.raidenmap.statecacher.TokenNetworkSnapshot;
-import io.raidenmap.statecacher.UserCount;
+import io.raidenmap.statecacher.*;
 import org.apache.kafka.common.serialization.Serde;
 
 import java.util.Collections;
 import java.util.Map;
 
 public class SpecificSerdeManager {
+
+    private Serde<TokenNetworkSnapshot> tokenNetworkSnapshotSerde1;
 
     public SpecificSerdeManager(String schemaRegistry) {
         serdeConfig = Collections.singletonMap("schema.registry.url", schemaRegistry);
@@ -29,6 +30,10 @@ public class SpecificSerdeManager {
         addSerdeConfig(producerKeySerde, true);
         keySerde = new SpecificAvroSerde<>();
         addSerdeConfig(keySerde, true);
+        endpointSerde = new SpecificAvroSerde<>();
+        addSerdeConfig(endpointSerde, false);
+        addressRegisteredSerde = new SpecificAvroSerde<>();
+        addSerdeConfig(addressRegisteredSerde, false);
         tokenNetworkCreatedSerde = new SpecificAvroSerde<>();
         addSerdeConfig(tokenNetworkCreatedSerde, false);
         channelOpenedSerde = new SpecificAvroSerde<>();
@@ -37,12 +42,16 @@ public class SpecificSerdeManager {
         addSerdeConfig(channelNewDepositSerde, false);
         channelClosedSerde = new SpecificAvroSerde<>();
         addSerdeConfig(channelClosedSerde, false);
-        channelSettledSerde= new SpecificAvroSerde<>();
+        channelSettledSerde = new SpecificAvroSerde<>();
         addSerdeConfig(channelSettledSerde, false);
         tokenNetworkDeltaSerde = new SpecificAvroSerde<>();
         addSerdeConfig(tokenNetworkDeltaSerde, false);
         tokenNetworkSnapshotSerde = new SpecificAvroSerde<>();
         addSerdeConfig(tokenNetworkSnapshotSerde, false);
+        raidenDeltaSerde = new SpecificAvroSerde<>();
+        addSerdeConfig(raidenDeltaSerde, false);
+        raidenSnapshotSerde = new SpecificAvroSerde<>();
+        addSerdeConfig(raidenSnapshotSerde, false);
 
         userCountSerde = new SpecificAvroSerde<>();
         addSerdeConfig(userCountSerde, false);
@@ -60,6 +69,12 @@ public class SpecificSerdeManager {
     public Serde<Key> getKeySerde() {
         return keySerde;
     }
+
+    public Serde<Endpoint> getEndpointSerde() {
+        return endpointSerde;
+    }
+
+    public Serde<AddressRegistered> getAddressRegisteredSerde(){ return addressRegisteredSerde;}
 
     public Serde<TokenNetworkCreated> getTokenNetworkCreatedSerde() {
         return tokenNetworkCreatedSerde;
@@ -93,17 +108,33 @@ public class SpecificSerdeManager {
         return userCountSerde;
     }
 
+    public Serde<RaidenDelta> getRaidenDeltaSerde() {
+        return raidenDeltaSerde;
+    }
+
+    public Serde<RaidenSnapshot> getRaidenSnapshotSerde() {
+        return raidenSnapshotSerde;
+    }
+
     private Map<String, String> serdeConfig;
+
     protected Serde<ProducerKey> producerKeySerde;
     protected Serde<Key> keySerde;
+
+    protected Serde<Endpoint> endpointSerde;
+    protected Serde<AddressRegistered> addressRegisteredSerde;
+    protected Serde<TokenNetworkCreated> tokenNetworkCreatedSerde;
     protected Serde<ChannelOpened> channelOpenedSerde;
     protected Serde<ChannelNewDeposit> channelNewDepositSerde;
     protected Serde<ChannelClosed> channelClosedSerde;
     protected Serde<ChannelSettled> channelSettledSerde;
 
-    protected Serde<TokenNetworkCreated> tokenNetworkCreatedSerde;
     protected Serde<TokenNetworkDelta> tokenNetworkDeltaSerde;
     protected Serde<TokenNetworkSnapshot> tokenNetworkSnapshotSerde;
+    protected Serde<RaidenDelta> raidenDeltaSerde;
+    protected Serde<RaidenSnapshot> raidenSnapshotSerde;
 
     protected Serde<UserCount> userCountSerde;
+
+
 }
